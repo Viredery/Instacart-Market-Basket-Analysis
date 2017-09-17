8018512c6b3528608d158e7d9e414bb830defa4a
Viredery <dyrviredery@hotmail.com>
2017-09-17 16:49:39 +0800

    Skip-Gram with Negative Sampling

--- rnn_solution/src/model/sgns.py
+++ rnn_solution/src/model/sgns.py
@@ -10,16 +10,16 @@ sys.path.append('../')
 from param_config import config
 from data_frame import DataFrame
 
+
 class DataReader(object):
     
     def __init__(self, data):
-        columns = ['i', 'j', 'V_ij']
+        columns = ['x', 'y']
         df = DataFrame(columns=columns, data=data)
         self.train_df, self.val_df = df.train_test_split(train_size=0.9, random_state=config.random_seed)
-        self.test_df = df
 
-        self.num_users = df['i'].max() + 1
-        self.num_products = df['j'].max() + 1
+        self.num_products = df['y'].max() + 1
+        self.product_dist = np.bincount(df['y']).tolist()
 
     def train_batch_generator(self, batch_size):
         return self.batch_generator(df=self.train_df, batch_size=batch_size, shuffle=True, num_epochs=10000)
@@ -31,58 +31,66 @@ class DataReader(object):
         return df.batch_generator(batch_size=batch_size, shuffle=shuffle, num_epochs=num_epochs, allow_smaller_final_batch=is_test)
 
 
-
-
-class NNMF(object):
-    """Non-Negative Matrix Factorization (NNMF)
+class SGNS(object):
+    """Skip-Gram with Negative Sampling
     """
 
     def __init__(self, 
                  data_reader,
-                 rank, 
-                 learning_rate=0.005, 
-                 batch_size=4096,
-                 num_training_steps=150000, 
-                 early_stopping_steps=30000,
-                 log_interval=200):
+                 embedding_size,
+                 num_sampled,
+                 learning_rate=0.002, 
+                 batch_size=64,
+                 num_training_steps=10*10**6, 
+                 early_stopping_steps=100000,
+                 log_interval=500):
 
         self.reader = data_reader
-        self.rank = rank
+        self.embedding_size = embedding_size
+        self.num_sampled = num_sampled
+
         self.learning_rate = learning_rate
         self.batch_size = batch_size
         self.num_training_steps = num_training_steps
         self.early_stopping_steps = early_stopping_steps
         self.log_interval = log_interval
 
+        self.vocabulary_size = self.reader.num_products
+
 
     def fit(self):
-        self.i = tf.placeholder(dtype=tf.int32, shape=[None])
-        self.j = tf.placeholder(dtype=tf.int32, shape=[None])
-        self.V_ij = tf.placeholder(dtype=tf.float32, shape=[None])
+        self.x = tf.placeholder(tf.int32, [None])
+        self.y = tf.placeholder(tf.int32, [None])
 
-        self.W = tf.Variable(tf.truncated_normal([self.reader.num_users, self.rank]))
-        self.H = tf.Variable(tf.truncated_normal([self.reader.num_products, self.rank]))
-        W_bias = tf.Variable(tf.truncated_normal([self.reader.num_users]))
-        H_bias = tf.Variable(tf.truncated_normal([self.reader.num_products]))
+        self.embeddings = tf.Variable(
+                tf.random_uniform([self.vocabulary_size, self.embedding_size], -1.0, 1.0))
+        nce_weights = tf.Variable(
+                tf.truncated_normal([self.vocabulary_size, self.embedding_size], stddev=1.0 / math.sqrt(self.embedding_size)))
+        nce_biases = tf.Variable(tf.zeros([self.vocabulary_size]))
 
-        global_mean = tf.Variable(0.0)
+        embed = tf.nn.embedding_lookup(self.embeddings, self.x)
 
-        w_i = tf.gather(self.W, self.i)
-        h_j = tf.gather(self.H, self.j)
+        sampled_values = tf.nn.fixed_unigram_candidate_sampler(
+            true_classes=tf.cast(tf.reshape(self.y, (-1, 1)), tf.int64),
+            num_true=1,
+            num_sampled=self.num_sampled,
+            unique=True,
+            range_max=self.vocabulary_size,
+            distortion=0.75,
+            unigrams=self.reader.product_dist
+        )
 
-        w_bias = tf.gather(W_bias, self.i)
-        h_bias = tf.gather(H_bias, self.j)
+        loss = tf.reduce_mean(
+                tf.nn.nce_loss(weights=nce_weights,
+                               biases=nce_biases,
+                               labels=self.y,
+                               inputs=embed,
+                               num_sampled=self.num_sampled,
+                               num_classes=self.vocabulary_size,
+                               sampled_values=sampled_values))
 
-        interaction = tf.reduce_sum(w_i * h_j, reduction_indices=1)
-        preds = global_mean + w_bias + h_bias + interaction
+        train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
 
-
-        rmse = tf.sqrt(tf.reduce_mean(tf.squared_difference(preds, self.V_ij)))
-        train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(rmse)
-
-        clip_W = self.W.assign(tf.maximum(tf.zeros_like(self.W), self.W))
-        clip_H = self.H.assign(tf.maximum(tf.zeros_like(self.H), self.H))
-        clip = tf.group(clip_W, clip_H)
 
         self.session = tf.Session()
         with self.session.as_default():
@@ -104,18 +112,16 @@ class NNMF(object):
                 val_feed_dict = { getattr(self, placeholder_name, None): data
                                     for placeholder_name, data in val_batch_df if hasattr(self, placeholder_name)}
                 
-                _, val_loss = self.session.run(fetches=[clip, rmse], feed_dict=val_feed_dict)
-
+                [val_loss] = self.session.run(fetches=[loss], feed_dict=val_feed_dict)
+                #print(val_loss)
                 val_loss_history.append(val_loss)
-                
+            
                 # train step
                 train_batch_df = next(train_generator)
                 train_feed_dict = { getattr(self, placeholder_name, None): data
                                     for placeholder_name, data in train_batch_df if hasattr(self, placeholder_name)}
 
-                self.session.run(fetches=[train_step], feed_dict=train_feed_dict)
-                _, train_loss = self.session.run(fetches=[clip, rmse], feed_dict=train_feed_dict)
-
+                train_loss, _ = self.session.run(fetches=[loss, train_step], feed_dict=train_feed_dict)
                 train_loss_history.append(train_loss)
 
                 if step % self.log_interval == 0:
@@ -132,28 +138,38 @@ class NNMF(object):
                 step += 1
 
     def predict(self):
-        np.save('{}/user_embeddings.npy'.format(config.data_store_path), self.W.eval(self.session))
-        np.save('{}/product_embeddings.npy'.format(config.data_store_path), self.H.eval(self.session))
-
+        np.save('{}/word2vec_product_embeddings.npy'.format(config.data_store_path), self.embeddings.eval(self.session))
 
 if __name__ == '__main__':
 
-    prior_products = pd.read_csv(config.raw_order_products_prior_path, usecols=['order_id', 'product_id'])
-    orders = pd.read_csv(config.raw_orders_path, usecols=['user_id', 'order_id'])
+    user_data = pd.read_csv(config.users_path)
 
-    user_products = prior_products.merge(orders, how='left', on='order_id')
+    x = []
+    y = []
+    for _, row in user_data.iterrows():
+        if _ % 10000 == 0:
+            print(_)
 
-    counts = user_products.groupby(['user_id', 'product_id']).size().rename('count').reset_index()
+        user_id = row['user_id']
+        products = row['product_ids']
+        products = ' '.join(products.split()[:-1])
+        for order in products.split():
+            items = order.split('_')
+            for i in range(len(items)):
+                for j in range(max(0, i - 2), min(i + 3, len(items))): # window = 5
+                    if i != j:
+                        x.append(int(items[i])) # input
+                        y.append(int(items[j])) # label (context)
 
-    i = counts['user_id'].values.copy()
-    j = counts['product_id'].values.copy()
-    V_ij = counts['count'].values.copy()
 
-    dr = DataReader(data=[i, j, V_ij])
-    nnmf = NNMF(data_reader=dr, rank=25)
+    x = np.array(x)
+    y = np.array(y)
 
-    del prior_products, orders, user_products, counts, i, j, V_ij
 
-    nnmf.fit()
-    nnmf.predict()
+    dr = DataReader(data=[x, y])
+    sgns = SGNS(data_reader=dr, embedding_size=25, num_sampled=100)
 
+    del user_data, x, y
+
+    sgns.fit()
+    sgns.predict()
